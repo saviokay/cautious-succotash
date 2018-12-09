b4b64ea7e59c5193bd8a19b49423d80c59d04696
saviokay <saviosebastian08@gmail.com>
2018-12-09 18:55:21 -0500

    Add Config.JS File To Store Access Token Information Seperately.

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    consumer_key:         'uIrHp5RM9ZI1tFze8OcqXI4Xa',
+    consumer_secret:      'XHIvWJYmQXR3r8uOiCQ5uy7c9gMZaeCkQfNUXwtomp6nLUOcMm',
+    access_token:         '1071907307452289025-ZOeuUF2huW8U3doyAvUUj194JyPoD5',
+    access_token_secret:  'csROlCCl85McXHkuw3pYhfu4kiNYNEfCKgFF6BeCvvFIx',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+
+}
