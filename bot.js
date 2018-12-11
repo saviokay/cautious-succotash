c5838af4351ba4237fd425ae884a53478a550516
saviokay <saviosebastian08@gmail.com>
2018-12-10 22:53:18 -0500

    Add Twitter Stream API Capabilities.

--- bot.js
+++ bot.js
@@ -45,3 +45,14 @@ function tweetIt() {
 	    console.log("It worked!");
 	  }
 	}
+
+    // Setting up a user stream
+    var stream = T.stream('user');
+
+    // To ensure we don't tweet ourselves, we must get our user data.
+    var my_screen_name = null;
+
+    T.get('account/verify_credentials', { skip_status: true }, function(err, data, response) {
+      my_screen_name = data.screen_name;
+      console.log('Using account ' + my_screen_name);
+    });
