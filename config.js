f132f3f1dd0aded241aa5e7652c71a4d8afdc0e6
Savio Kay <saviosebastian08@gmail.com>
2019-03-14 23:31:17 -0400

    Update Config.js
    Making changes for testing.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     consumer_secret:      'XHIvWJYmQXR3r8uOiCQ5uy7c9gMZaeCkQfNUXwtomp6nLUOcMm',
     access_token:         '1071907307452289025-ZOeuUF2huW8U3doyAvUUj194JyPoD5',
     access_token_secret:  'csROlCCl85McXHkuw3pYhfu4kiNYNEfCKgFF6BeCvvFIx',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+    timeout_ms:           70*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            false,     // optional - requires SSL certificates to be valid.
 
 }
